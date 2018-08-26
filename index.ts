7bf05275d0169ea18c9e740eb0319a6a03bfbdfd
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2018-08-26 09:51:54 +0530

    init

---
+++ index.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('PXE sample app running')
+});
+
+app.listen(8080, () => {
+    console.log("sample app started on http://localhost:8080");
+});
