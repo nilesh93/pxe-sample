8fcac852cb7d7ac082f8e45e8097af570f5ead46
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2018-09-27 07:40:20 +0530

    minor change

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send(`PXE sample app running DEMO DEMO cloned from github. DEMO Environment Variable set for [env_name] is: ${process.env.env_name}`)
+    res.send(`PXE sample app running HELLO cloned from github. DEMO Environment Variable set for [env_name] is: ${process.env.env_name}`)
 });
 
 app.listen(8080, () => {
