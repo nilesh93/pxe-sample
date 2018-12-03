7ded9abedf506e4fb100ad3174ca12c6fe4f43b1
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2018-12-03 19:18:34 +0530

    minor fix

--- index.ts
+++ index.ts
@@ -5,7 +5,8 @@ import { CONFIG_SECRET } from "./config.secret";
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
-
+var formidable = require('formidable');
+var fs = require('fs');
 
 const app = express();
 
@@ -14,9 +15,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    
-    res.send(`
+
+app.get('/', async (req, res) => {
+    let html = `
+    <h2> PXE Basic Info </h2>
     Hello PXE   Welcome to the Demo on Oct 23rd <br>       
     PXE sample app running  cloned from github.  <br>
     Environment Variable set for [env_name] is: ${process.env.env_name}. <br>
@@ -24,10 +26,61 @@ app.get('/', (req, res) => {
     APP VERSION: ${CONFIG.app_version} <br>
     Encrypted VERSION: ${CONFIG_SECRET.app_version} <br>
     Platformer image version: ${process.env.PLATFORMER_IMG_TAG}
-    Hello mahesh
-    `)
+    <br>
+    --------------------------------
+    <h2> Upload Test Form </h2>
+    <form action="/" enctype="multipart/form-data" method="post">
+    <input type="file" name="upload" multiple>
+    <input type="submit" value="Upload">
+    </form>
+    <br>
+    ================================
+    <br> 
+    <h2> Uploaded files </h2>
+    <ul>
+    `;
+
+
+    html = <string>await getUploads(html);
+
+    html += '</ul>';
+
+    res.send(html)
 });
 
-app.listen(8080, () => {
+
+
+app.post('/', function (req, res) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file) {
+        file.path = __dirname + '/uploads/' + file.name;
+    });
+
+    form.on('file', function (name, file) {
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.redirect('/');
+});
+
+
+app.listen(8081, () => {
     console.log("sample app started on  http://localhost:8080");
 });
+
+
+async function getUploads(html) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(__dirname + '/uploads/', function (err, items) {
+            console.log(items);
+            for (var i = 0; i < items.length; i++) {
+                console.log(items[i]);
+                html += `<li> <a href="/uploads/${items[i]}" target="_blank"> ${items[i]} </li>`;
+            }
+            resolve(html);
+        });
+    });
+}
